5605141280fca4e2de539447f40e2698b2d0f6e4
Tonnienc <tonniegitonga@gmail.com>
2023-09-07 12:43:22 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,12 @@
 /**
  * main - a c program that prints to the standard error
  * Return 1 (its successful
- */
+*/
+
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+
 }
+
