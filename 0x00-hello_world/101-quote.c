3904cf02b586aaae369b044fe57627b17aa939c5
Tonnienc <tonniegitonga@gmail.com>
2023-09-07 12:47:53 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a c program that prints to the standard error
- * Return 1 (its successful
+ * main - Entry Point
+ * Return 1 (its successful)
 */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 
 }
