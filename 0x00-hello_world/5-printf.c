e864869b3338e1948a10aa0468bcb466629f7070
Tonnienc <tonniegitonga@gmail.com>
2023-09-07 10:48:46 -0700

    printsf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - A program that prints a line using printf function
+ * Return: 0 (works succesfully)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
