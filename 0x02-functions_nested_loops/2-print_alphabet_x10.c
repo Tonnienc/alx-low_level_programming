c380e9bbf4eb258e4879214fa4952d9bbcb7bfee
Tonnienc <tonniegitonga@gmail.com>
2023-09-13 04:23:45 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
 */
 void print_alphabet_x10(void)
 {
-	char ch;
+	char c;
 	int i;
 
 	i = 0;
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		c = 'a';
+		while (c <= 'z')
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
 		i++;
