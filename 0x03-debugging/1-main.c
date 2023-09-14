ec0b3c8ee501d51903aeda5bbd6e7db2e53ab274
Tonnienc <tonniegitonga@gmail.com>
2023-09-14 14:19:12 -0700

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#include <stdio.h>
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
 int main(void)
 {
 	int i;
